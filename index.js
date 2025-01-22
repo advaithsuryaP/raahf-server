4af1fc3edd376109d4f7cef9f0e08a8df3dd7b9a
advaithsuryaP <padvaithsurya@gmail.com>
2025-01-23 00:50:22 +0530

    initialize server setup with Express, add Prettier and EditorConfig configurations

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
